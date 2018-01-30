db030c8b27aab3a6b02ecdcb3af1ecfd59fca914
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-30 18:58:40 +0100

    Refactor cryptos/crypto route

--- app/routes/cryptos/crypto.js
+++ app/routes/cryptos/crypto.js
@@ -1,9 +1,6 @@
 import Route from '@ember/routing/route';
-import RSVP from 'rsvp';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  router: service(),
   model(params){
     const symbol = params.crypto_symbol.toUpperCase(),
           crypto = this.modelFor('cryptos')
@@ -12,8 +9,7 @@ export default Route.extend({
   },
   afterModel(model, transition){
     if(transition.targetName !== 'cryptos.crypto.option'){
-      this.get('router').transitionTo('cryptos.crypto.option', 'price_usd');
+      this.transitionTo('cryptos.crypto.option', 'price_usd');
     }
   },
-
 });
