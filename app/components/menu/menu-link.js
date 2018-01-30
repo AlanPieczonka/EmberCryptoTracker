67467a7b10960a811942ab81aa40606dc64ac806
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-30 19:11:58 +0100

    Clean up menu/menu-link component

--- app/components/menu/menu-link.js
+++ app/components/menu/menu-link.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
-import RSVP from 'rsvp';
 import $ from 'jquery';
 
 const resetSelect = () => {
@@ -14,7 +13,7 @@ export default Component.extend({
   router: service(),
   tagName: 'a',
   click(){
-    resetSelect();
+   resetSelect();
    get(this, 'router').transitionTo('cryptos.crypto', get(this, 'crypto').symbol);
   },
   classNames: ['ui', 'primary', 'basic', `button`], 
