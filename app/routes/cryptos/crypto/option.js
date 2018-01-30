840081d87cd2e8b9d3464e1f92881ad4d63781d9
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-30 19:00:17 +0100

    Refactor cryptos/crypto/option route

--- app/routes/cryptos/crypto/option.js
+++ app/routes/cryptos/crypto/option.js
@@ -1,10 +1,8 @@
 import Route from '@ember/routing/route';
-import { set } from '@ember/object';
 import RSVP from 'rsvp';
 
 export default Route.extend({
   model(params){
-    console.log(params.option);
     return RSVP.hash({
       crypto: this.modelFor('cryptos.crypto'),
       option: params.option
