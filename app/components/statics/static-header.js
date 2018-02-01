3577282287cd7b27fcf9f4bb13faabd09826c860
Alan Pieczonka <alanpieczonka@gmail.com>
2018-02-01 19:02:49 +0100

    Make some styling improvements

--- app/components/statics/static-header.js
+++ app/components/statics/static-header.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'h1',
-  classNames: ['ui', 'center', 'aligned', 'header', 'margin--30'],
+  classNames: ['ui', 'center', 'aligned', 'header', 'margin--30', 'centered', 'weight--300'],
 });
--- app/routes/cryptos.js
+++ app/routes/cryptos.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(){
-    return fetch('https://api.coinmarkeftcap.com/v1/ticker/?limit=8')
+    return fetch('https://api.coinmarketcap.com/v1/ticker/?limit=8')
           .then(response => response.json())
           .catch(err => console.error(err))
   }
